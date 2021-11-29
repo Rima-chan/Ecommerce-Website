ccc0a50899a54639ffca868f81ffa2d959e714d1
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-11-29 19:45:51 +0100

    Test config.json link

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,5 +4,5 @@ async function loadConfig(path) {
     return result.json();
 }
 
-let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? "http://localhost:3000":"https://orinoco-mb.herokuapp.com";
+let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? "http://localhost:3000":"https://orinoco-back-mb.herokuapp.com";
 
